cec1c94c8213f2c1976475a44a645f590a1bafe8
vananh-287 <164316789+vananh-287@users.noreply.github.com>
2025-04-23 03:41:53 +0700

    Update Loai.java

--- src/cua_hang_tien_loi/entity/Loai.java
+++ src/cua_hang_tien_loi/entity/Loai.java
@@ -1,5 +1,34 @@
-package cua_hang_tien_loi.entity;
+package cua_hang_tien_loi.entity ;
 
 public class Loai {
+	private String maLoai;
+    private String tenLoai;
+
+    public Loai(String maLoai, String tenLoai) {
+        this.maLoai = maLoai;
+        this.tenLoai = tenLoai;
+    }
+
+	public String getMaLoai() {
+		return maLoai;
+	}
+
+	public void setMaLoai(String maLoai) {
+		this.maLoai = maLoai;
+	}
+
+	public String getTenLoai() {
+		return tenLoai;
+	}
+
+	public void setTenLoai(String tenLoai) {
+		this.tenLoai = tenLoai;
+	}
+
+	@Override
+	public String toString() {
+		return "Loai [maLoai=" + maLoai + ", tenLoai=" + tenLoai + "]";
+	}
+    
 
 }
